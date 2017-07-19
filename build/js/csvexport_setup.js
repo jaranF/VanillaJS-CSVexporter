0f90235d621c5f1167bf7e507987468f7a3e97fc
Jaran F <jaranMcLarn@gmail.com>
2017-07-19 02:41:50 +0100

    A few Linting and whitespace improvements

--- build/js/csvexport_setup.js
+++ build/js/csvexport_setup.js
@@ -1,10 +1,17 @@
+/*global angular: false, jstestdriver: false, beforeEach: false, afterEach: false, inject: false, andExecuteWith: false, spyOn: false, describe: false, beforeEach: false, inject: false, it: false, expect: false, module: false, : false, app: false, exportCSV: false */
+/*jslint newcap: true, white: true, sloppy: true, vars: true, unparam: true */
+
+/**
+ * @author Jaran F
+ * @description To demonstrate the 'CSVexport.js' functionality there the need for convenience in having 'pre-baked' sample JSON data that can be reviewed (in this case because it is in a <TEXTAREA> element) and picked up from DOM before it is converted to CSV. 
+ */
 window.onload = function() {
   var d = document, eleForm = d.getElementById("jsonPOSTform");
 
   function pickUpJSONandConvert() {
     var objJSON = JSON.parse(d.getElementById("data").value);
     var sfileName = d.getElementById("filenameString").value;
-    var isExcelFormat = !!(eleForm.elements["isExcelFormatExport"].value - 0)
+    var isExcelFormat = !!(eleForm.elements["isExcelFormatExport"].value - 0);
     var mock = {
       ENVINFO: {
         browser: {
@@ -38,6 +45,7 @@ window.onload = function() {
     };
     var sConvertedToCSV = app.exportCSV.inject([mock, fnSafariDownloadfallback]).andExecuteWith(objJSON, sfileName, isExcelFormat);
     d.getElementById("csv").value = sConvertedToCSV;
+    return sConvertedToCSV;
   } //End #pickUpJSONandConvert()'
   if (window.attachEvent) {
     eleForm.attachEvent("onsubmit", function(evt) {
